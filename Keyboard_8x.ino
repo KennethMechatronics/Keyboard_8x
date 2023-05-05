b5fc2157306545a3d202e64c13c7b0e2db0ebd9d
KennethMechatronics <118351204+KennethMechatronics@users.noreply.github.com>
2023-05-05 18:15:49 +0200

    Update Keyboard_8x.ino

--- Keyboard_8x.ino
+++ Keyboard_8x.ino
@@ -19,7 +19,7 @@ const int knapp8 = 9;
 unsigned long fortid;
 int debounceDelay = 100;
 
-int sepDel = 400;
+int sepDel = 10;
 
 
 void setup() {
@@ -31,11 +31,11 @@ void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  for (int j = 0; j <= 9; j++) {
+  for (int j = 0; j <= 2; j++) {
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
+    delay(sepDel);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
+    delay(sepDel);
   }
 
   delay(2000);
@@ -46,25 +46,34 @@ void loop() {
 
   if (digitalRead(knapp1) == LOW) {
     knapp1Trykket();
+    delay(300);
   } else if (digitalRead(knapp2) == LOW) {
     knapp2Trykket();
+    delay(300);
   } else if (digitalRead(knapp3) == LOW) {
     knapp3Trykket();
+    delay(300);
   } else if (digitalRead(knapp4) == LOW) {
     knapp4Trykket();
+    delay(300);
   } else if (digitalRead(knapp5) == LOW) {
     knapp5Trykket();
+    delay(300);
   } else if (digitalRead(knapp6) == LOW) {
     knapp6Trykket();
+    delay(300);
   } else if (digitalRead(knapp7) == LOW) {
     knapp7Trykket();
+    delay(300);
   } else if (digitalRead(knapp8) == LOW) {
     knapp8Trykket();
+    delay(300);
   }
 }
 
 
 void knapp1Trykket() {  //DIAGNOSE
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("+00");
   Keyboard.write(KEY_F3);
   delay(sepDel);
@@ -81,9 +90,11 @@ void knapp1Trykket() {  //DIAGNOSE
   Keyboard.write(KEY_RETURN);
   delay(sepDel);
   Keyboard.write(KEY_ESC);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void knapp2Trykket() {  // Referanser
+  digitalWrite(LED_BUILTIN, HIGH);
   Keyboard.write(KEY_LEFT_ARROW);
   delay(sepDel);
   Keyboard.write(KEY_LEFT_ARROW);
@@ -110,10 +121,15 @@ void knapp2Trykket() {  // Referanser
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.print("v");
   Keyboard.releaseAll();
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void knapp3Trykket() {  // Not printed
+  digitalWrite(LED_BUILTIN, HIGH);
   Keyboard.write(KEY_TAB);
+  delay(sepDel);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
   delay(sepDel);
   Keyboard.press(KEY_DOWN_ARROW);
   Keyboard.releaseAll();
@@ -124,24 +140,54 @@ void knapp3Trykket() {  // Not printed
   Keyboard.press(KEY_LEFT_SHIFT);
   Keyboard.write(KEY_RETURN);
   Keyboard.releaseAll();
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void knapp4Trykket() {  // 0,5 Fett
-  Keyboard.write(KEY_F3);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Keyboard.write(KEY_F2);
   delay(sepDel);
   Keyboard.println("1006529");
   delay(sepDel);
   Keyboard.println("0,5");
   delay(sepDel);
   Keyboard.write(KEY_ESC);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void knapp5Trykket() {
-  // Noe kode
+void knapp5Trykket() {  // Lime inn arbeidskode
+  Keyboard.write(KEY_F3);
+  delay(sepDel);
+  Keyboard.press(KEY_LEFT_CTRL);
+  delay(sepDel);
+  Keyboard.print("v");
+  Keyboard.releaseAll();
+  delay(sepDel);
+  Keyboard.write(KEY_RETURN);
+  delay(sepDel);
+  Keyboard.write(KEY_RETURN);
+  delay(sepDel);
 }
 
-void knapp6Trykket() {
-  // Noe kode
+void knapp6Trykket() {  // Ramas
+  Keyboard.press(KEY_LEFT_CTRL);
+  delay(sepDel);
+  Keyboard.print("j");
+  Keyboard.releaseAll();
+  delay(sepDel);
+  Keyboard.write(KEY_LEFT_ARROW);
+  delay(sepDel);
+  Keyboard.write(KEY_LEFT_ARROW);
+  delay(sepDel);
+  Keyboard.write(KEY_RETURN);
+  delay(sepDel);
+  Keyboard.write(KEY_TAB);
+  delay(sepDel);
+  Keyboard.write(KEY_TAB);
+  delay(sepDel);
+  Keyboard.println("62055");
+  delay(sepDel);
+  Keyboard.write(KEY_RETURN);
 }
 
 void knapp7Trykket() {
@@ -150,7 +196,7 @@ void knapp7Trykket() {
 
 void knapp8Trykket() {
   delay(1000);
-  sepDel = sepDel + 200;
+  sepDel = sepDel * 2;
   Serial.println("Oker sepDel til " + String(sepDel));
   for (int i = 0; i <= 2; i++) {
     digitalWrite(LED_BUILTIN, HIGH);
@@ -159,7 +205,7 @@ void knapp8Trykket() {
     delay(sepDel);
   }
   if (sepDel >= 2000) {
-    sepDel = 200;
+    sepDel = 2;
   }
 }
 
