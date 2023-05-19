cc691930f3d9f0de569c2ae953dc70ec6d06389d
KennethMechatronics <118351204+KennethMechatronics@users.noreply.github.com>
2023-05-19 10:48:04 +0200

    Update Keyboard_8x.ino

--- Keyboard_8x.ino
+++ Keyboard_8x.ino
@@ -1,11 +1,13 @@
-/* Filnavn: Keyboard_8x
+/* Filnavn: Keyboard_8x.ino
   Versjon: V1.0
-  Revisjon: V1.0 - 04.05.23 - Første versjon - Kenneth Paulsen 
+  Revisjon: 5 - 14.05.23 - Kenneth Paulsen 
 
   Program:
+  Dette programmet er laget for å fungere som et macrokeyboard med macroer for nyttige tastetrykk i min nåværende jobb. 
+  Macroene byttes med jevne mellomrom ut da en encoder for å bytte layout ennå ikke er loddet på kretskortet. 
 */
 #include "Keyboard.h"
-#include "HID.h"
+
 
 const int knapp1 = 2;
 const int knapp2 = 3;
@@ -16,10 +18,7 @@ const int knapp6 = 7;
 const int knapp7 = 8;
 const int knapp8 = 9;
 
-unsigned long fortid;
-int debounceDelay = 100;
-
-int sepDel = 10;
+int dly = 10;
 
 
 void setup() {
@@ -33,179 +32,234 @@ void setup() {
 
   for (int j = 0; j <= 2; j++) {
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(sepDel);
+    delay(dly);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(sepDel);
+    delay(dly);
   }
 
   delay(2000);
 }
 
 void loop() {
-  unsigned long naatid = millis();
 
   if (digitalRead(knapp1) == LOW) {
-    knapp1Trykket();
+    digitalWrite(LED_BUILTIN, HIGH);
+    diagnose();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   } else if (digitalRead(knapp2) == LOW) {
-    knapp2Trykket();
+    digitalWrite(LED_BUILTIN, HIGH);
+    referanser();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   } else if (digitalRead(knapp3) == LOW) {
-    knapp3Trykket();
+    digitalWrite(LED_BUILTIN, HIGH);
+    notPrinted();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   } else if (digitalRead(knapp4) == LOW) {
-    knapp4Trykket();
+    digitalWrite(LED_BUILTIN, HIGH);
+    fett();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   } else if (digitalRead(knapp5) == LOW) {
-    knapp5Trykket();
+    digitalWrite(LED_BUILTIN, HIGH);
+    limeArbKode();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   } else if (digitalRead(knapp6) == LOW) {
-    knapp6Trykket();
+    digitalWrite(LED_BUILTIN, HIGH);
+    ramas();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   } else if (digitalRead(knapp7) == LOW) {
-    knapp7Trykket();
+    digitalWrite(LED_BUILTIN, HIGH);
+    smoreHenger();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   } else if (digitalRead(knapp8) == LOW) {
+    digitalWrite(LED_BUILTIN, HIGH);
     knapp8Trykket();
     delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
 
 
-void knapp1Trykket() {  //DIAGNOSE
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("+00");
+void diagnose() {  //DIAGNOSE
   Keyboard.write(KEY_F3);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_TAB);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_KP_PLUS);
-  delay(sepDel);
+  delay(dly);
   Keyboard.println("00");
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_DOWN_ARROW);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_RETURN);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_RETURN);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_ESC);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void knapp2Trykket() {  // Referanser
+void referanser() {  // Referanser
   digitalWrite(LED_BUILTIN, HIGH);
   Keyboard.write(KEY_LEFT_ARROW);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_LEFT_ARROW);
   for (int t = 0; t <= 7; t++) {
     Keyboard.write(KEY_TAB);
-    delay(sepDel);
+    delay(dly);
   }
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.print("c");
   Keyboard.releaseAll();
   Keyboard.press(KEY_LEFT_SHIFT);
-  delay(sepDel);
+  delay(dly);
   for (int sT = 0; sT <= 7; sT++) {
     Keyboard.write(KEY_TAB);
-    delay(sepDel);
+    delay(dly);
   }
   Keyboard.releaseAll();
   Keyboard.write(KEY_RIGHT_ARROW);
-  delay(sepDel);
+  delay(dly);
   for (int t = 0; t <= 3; t++) {
     Keyboard.write(KEY_TAB);
-    delay(sepDel);
+    delay(dly);
   }
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.print("v");
   Keyboard.releaseAll();
+  delay(dly);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  for (int i = 0; i <= 3; i++) {
+    Keyboard.write(KEY_TAB);
+    delay(dly);
+  }
+  Keyboard.releaseAll();
+  Keyboard.write(KEY_LEFT_ARROW);
+  delay(dly);
+  for (int i = 0; i <= 13; i++) {
+    Keyboard.write(KEY_TAB);
+    delay(dly);
+  }
+
+
+
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void knapp3Trykket() {  // Not printed
+void notPrinted() {  // Not printed
   digitalWrite(LED_BUILTIN, HIGH);
   Keyboard.write(KEY_TAB);
-  delay(sepDel);
+  delay(dly);
   Keyboard.press(KEY_DOWN_ARROW);
   Keyboard.releaseAll();
-  delay(sepDel);
+  delay(dly);
   Keyboard.press(KEY_DOWN_ARROW);
   Keyboard.releaseAll();
-  delay(sepDel);
+  delay(dly);
   Keyboard.press(KEY_DOWN_ARROW);
   Keyboard.releaseAll();
-  delay(sepDel);
+  delay(dly);
   Keyboard.press(KEY_LEFT_SHIFT);
   Keyboard.write(KEY_RETURN);
   Keyboard.releaseAll();
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void knapp4Trykket() {  // 0,5 Fett
+void fett() {  // 0,5 Fett
   digitalWrite(LED_BUILTIN, HIGH);
   Keyboard.write(KEY_F2);
-  delay(sepDel);
+  delay(dly);
   Keyboard.println("1006529");
-  delay(sepDel);
+  delay(dly);
   Keyboard.println("0,5");
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_ESC);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void knapp5Trykket() {  // Lime inn arbeidskode
+void limeArbKode() {  // Lime inn arbeidskode
   Keyboard.write(KEY_F3);
-  delay(sepDel);
+  delay(dly);
   Keyboard.press(KEY_LEFT_CTRL);
-  delay(sepDel);
+  delay(dly);
   Keyboard.print("v");
   Keyboard.releaseAll();
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_RETURN);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_RETURN);
-  delay(sepDel);
+  delay(dly);
+  Keyboard.write(KEY_RETURN);
+  delay(dly);
+  Keyboard.write(KEY_ESC);
 }
 
-void knapp6Trykket() {  // Ramas
+void ramas() {  // Ramas
   Keyboard.press(KEY_LEFT_CTRL);
-  delay(sepDel);
+  delay(dly);
   Keyboard.print("j");
   Keyboard.releaseAll();
-  delay(sepDel);
+  delay(dly * 4);
   Keyboard.write(KEY_LEFT_ARROW);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_LEFT_ARROW);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_RETURN);
-  delay(sepDel);
+  delay(dly * 2);
   Keyboard.write(KEY_TAB);
-  delay(sepDel);
+  delay(dly);
   Keyboard.write(KEY_TAB);
-  delay(sepDel);
+  delay(dly * 2);
   Keyboard.println("62055");
-  delay(sepDel);
+  delay(dly * 2);
   Keyboard.write(KEY_RETURN);
 }
 
-void knapp7Trykket() {
-  // Noe kode
+void smoreHenger() { // Smøre henger  248	F8	ø
+  Keyboard.write(KEY_F3);
+  delay(dly);
+  Keyboard.println("21008705");
+  for (int t = 0; t <= 17; t++) {
+    Keyboard.write(KEY_TAB);
+    delay(dly);
+  }
+  Keyboard.write(KEY_BACKSPACE);
+  delay(dly);
+  Keyboard.print("Smøre henger");
+  delay(dly);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  delay(dly);
+   for (int t = 0; t <= 5; t++) {
+    Keyboard.write(KEY_TAB);
+    delay(dly);
+  }
+  Keyboard.releaseAll();
+  delay(dly);
+  Keyboard.println("Smøre henger");
+  delay(dly);
+  Keyboard.write(KEY_ESC);
+  delay(dly);
+  fett();
 }
 
 void knapp8Trykket() {
-  delay(1000);
-  sepDel = sepDel * 2;
-  Serial.println("Oker sepDel til " + String(sepDel));
+  delay(200);
+  dly = dly * 2;
+  Serial.println("Oker dly til " + String(dly));
   for (int i = 0; i <= 2; i++) {
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(sepDel);
+    delay(dly);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(sepDel);
+    delay(dly);
   }
-  if (sepDel >= 2000) {
-    sepDel = 2;
+  if (dly >= 2000) {
+    dly = 2;
   }
 }
 
